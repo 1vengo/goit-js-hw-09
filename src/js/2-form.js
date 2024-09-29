fab43fe77826ac19932d82736c1fb79e1069d1aa
Ivan <159075250+1vengo@users.noreply.github.com>
2024-09-29 18:20:13 +0200

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,54 +1,39 @@
-  const form = document.querySelector('.feedback-form');
-  const emailInput = form.elements.email;
-  const messageInput = form.elements.message;
-  
-  const STORAGE_KEY = 'feedback-form-state';
-  
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
 
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-  if (savedData.email) emailInput.value = savedData.email;
-  if (savedData.message) messageInput.value = savedData.message;
-  
+const STORAGE_KEY = 'feedback-form-state';
 
-  const saveFormData = () => {
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+if (savedData.email) emailInput.value = savedData.email;
+if (savedData.message) messageInput.value = savedData.message;
+
+const saveFormData = () => {
     const formData = {
-      email: emailInput.value.trim(),
-      message: messageInput.value.trim(),
+        email: emailInput.value.trim(),
+        message: messageInput.value.trim(),
     };
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  };
-
-
-  form.addEventListener('input', (event) => {
+};
+form.addEventListener('input', (event) => {
     saveFormData();
-  });
-
-
-  form.addEventListener('submit', (event) => {
+});
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-
     const email = emailInput.value.trim();
     const message = messageInput.value.trim();
-
-
     if (!email || !message) {
-      alert('Please fill in both fields!');
-      return;
+        alert('Please fill in both fields')
+        return;
     }
-
-
     console.log({ email, message });
-
-
     localStorage.removeItem(STORAGE_KEY);
     form.reset();
-  });
-
-
-  form.addEventListener('input', (event) => {
+});
+form.addEventListener('input', (event) => {
     const currentData = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (!currentData) {
-      saveFormData();
+        saveFormData();
+        
     }
-  });
-;
+})
